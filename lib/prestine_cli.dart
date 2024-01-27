2e3d25baf13992482abbaea740793ff1c0db269a
Anil Thapa <anil.thapa@8squarei.com>
2024-01-27 16:44:11 +0545

    initial commit

---
+++ lib/prestine_cli.dart
@@ -0,0 +1,10 @@
+/// prestine_cli, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate prestine_cli
+/// dart pub global activate prestine_cli
+///
+/// # see usage
+/// prestine_cli --help
+/// ```
+library prestine_cli;
